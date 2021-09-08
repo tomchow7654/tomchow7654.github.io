8e0768fa3a100ebf9e4cc243ee17f88ce2690e8b
tom chow <tomchow7654@gmail.com>
2021-09-09 02:05:02 +0800

    added ability to specify durability, stack & wrapping paper

--- script.js
+++ script.js
@@ -10,14 +10,28 @@
           default: 'ACNH item id lookup',
           TWzh: "動森物品ID 尋找",
         },
-        itemData: {},
+        itemData: {
+          translated: {},
+          stack: {},
+          durability: [],
+          wrappingPaper: [],
+          variants: {
+            data: [],
+            internal_names: [],
+          },
+        },
+        search: {
+          text: "",
+          result: [],
+          resultVariants: [],
+          moreThan100: false,
+          openMenu: false,
+        },
         itemVariants: {
           open: "",
           data: [],
           result: [],
         },
-        results: [],
-        search: "",
         copyText: "",
         selected: {
           itemPrefix: "",
@@ -98,19 +112,54 @@
         if (variantId == 0) return itemId;
         else return "0000" + variantId.toString(16).toUpperCase().padStart(4, "0") + "0000" + itemId;
       },
-      diyToDropCmd(diyId) {
-        return diyId + "0000" + "16A2";
+      getBagPicture(item) {
+        let color = item.wrappingPaper.color;
+        if (color.length == 0) return "";
+        if (color == 'festive') color = 'ornament';
+        color = color.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+          if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+          return index === 0 ? match.toLowerCase() : match.toUpperCase();
+        });
+        return 'https://acnhcdn.com/latest/MenuIcon/WBag' + color.charAt(0).toUpperCase() + color.slice(1) + '.png';
       },
-      addToSelected(item, type = "item") {
-        if (type == "item") {
-          this.selected.items.push(item.id[1])
+      calculateItemId(item, opt = {}) {
+        let variantId = (opt && opt.variantId) ? opt.variantId : "",
+          isDiy = (opt && opt.isDiy) ? opt.isDiy : false,
+          command = (opt && opt.command) ? opt.command : "diy";
+        if (isDiy && command == "diy") return [item.DiyRecipe[1]];
+
+        let a = "0000", b = "0000", c = "0000", d = isDiy ? "16A2" : item.id[1];
+        if (!!item.durability)
+          a = (item.durability.max - item.durability.current).toString(16).toUpperCase().padStart(4, "0");
+
+        if (isDiy) b = item.DiyRecipe[1];
+        else if (!!item.stack) b = (item.stack.current - 1).toString(16).toUpperCase().padStart(4, "0");
+        else if (variantId.length > 0) b = variantId.toString(16).toUpperCase().padStart(4, "0");
+
+        if (item.wrappingPaper.color.length > 0) {
+          let color = this.itemData.wrappingPaper.find(c => c.color == item.wrappingPaper.color);
+          c = item.wrappingPaper.withName ? color.withName : color.hex;
         }
-        else if (type == "diy") {
-          this.selected.diys.push(item.DiyRecipe[1]);
-        }
-        else {
-          this.selected.items.push(this.diyToDropCmd(item.DiyRecipe[1]));
-        }
+        return [a, b, c, d];
+      },
+      trimItemId(ids, withSpace = false) {
+        if (!Array.isArray(ids)) return ids;
+        let trimmed = ids.map(i => i.toString(16).toUpperCase().padStart(4, "0")).join("").replace(/\b0+/g, '');
+        if (withSpace) return trimmed.replace(/\B(?=(\w{4})+(?!\w))/g, " ");
+        else return trimmed;
+      },
+      addToSelected(item, opt = {}) {
+        let variantId = (opt && opt.variantId) ? opt.variantId : "",
+          isDiy = (opt && opt.isDiy) ? opt.isDiy : false,
+          command = (opt && opt.command) ? opt.command : "diy";
+
+        let ids = this.calculateItemId(item, { variantId, isDiy, command });
+        if (isDiy && command == "diy") this.selected.diys.push(ids[0]);
+        else this.selected.items.push(ids);
+        this.saveToLocalStorage();
+      },
+      joinSelected(items) {
+        return items.map(i => this.trimItemId(i)).join(" ");
       },
       showAllToast() {
         if (this.pref.showToast)
@@ -124,30 +173,99 @@
           });
       },
       saveToLocalStorage() {
+        let pref = Object.assign({}, this.pref);
+        delete pref.language;
         let obj = {
           itemPrefix: this.selected.itemPrefix,
           diyPrefix: this.selected.diyPrefix,
           splitBy: this.selected.splitBy,
+          pref: pref,
           language: this.pref.language.selected,
-          searchText: this.search,
+          searchText: this.search.text,
+          selectedItems: this.selected.items,
+          selectedDiys: this.selected.diys,
         };
         localStorage.setItem("data", JSON.stringify(obj));
+      },
+      searchItems(text, language) {
+        this.search.results = [];
+
+        if (text.length > 0) {
+          Object.entries(this.itemData.translated).forEach(([key, val]) => {
+            if (this.search.results.length < 100)
+              this.search.results.push(...val.filter(item => item.name.toLowerCase().includes(text.toLowerCase())));
+          });
+          this.search.moreThan100 = this.search.results.length > 100;
+          if (this.search.results.length > 100)
+            this.search.results = this.search.results.slice(0, 100);
+
+          // processing
+          this.search.results.forEach(r => {
+            if (r.internal_name) {
+              Object.values(this.itemData.stack).forEach(arr => { // adding stack data
+                let i = arr.findIndex(x => x.internal_name == r.internal_name);
+                if (i > -1)
+                  r.stack = { current: arr[i].stack, max: arr[i].stack };
+              });
+
+              let toolIndex = this.itemData.durability.findIndex(tool => tool.internal_name == r.internal_name);
+              if (toolIndex > -1) r.durability = {
+                current: this.itemData.durability[toolIndex].durability,
+                max: this.itemData.durability[toolIndex].durability
+              };
+
+              if (this.itemData.variants.internal_names.includes(r.internal_name)) {
+                r.variants = this.itemData.variants.data.reduce((arr, v) => {
+                  if (v.id == r.internal_name) {
+                    v.variant_id_trimmed = v.variant_id.slice(v.variant_id.lastIndexOf("_") + 1);
+                    arr.push(v);
+                  }
+                  return arr;
+                }, []);
+              }
+            }
+            r.wrappingPaper = { color: "", withName: false };
+          });
+        }
+        this.saveToLocalStorage();
       },
     },
     created() {
       if (localStorage.getItem("data")) {
         let saved = JSON.parse(localStorage.getItem("data"));
-        this.selected.itemPrefix = saved.itemPrefix;
-        this.selected.diyPrefix = saved.diyPrefix;
-        this.selected.splitBy = saved.splitBy;
-        this.pref.language.selected = saved.language;
-        this.search = saved.searchText;
+        this.selected.itemPrefix = saved.itemPrefix || this.selected.itemPrefix;
+        this.selected.diyPrefix = saved.diyPrefix || this.selected.diyPrefix;
+        this.selected.splitBy = saved.splitBy || this.selected.splitBy;
+        if (saved.pref) Object.entries(saved.pref).forEach(([key, val]) => this.pref[key] = val);
+        this.pref.language.selected = saved.language || this.pref.language.selected;
+        this.search.text = saved.searchText || this.search.text;
+        this.selected.items = saved.selectedItems || this.selected.items;
+        this.selected.diys = saved.selectedDiys || this.selected.diys;
       }
       else if (navigator.language == "zh-TW") this.pref.language.selected = "TWzh";
 
       fetch("./translation-sheet-data/variants.json")
         .then(response => response.json())
-        .then(json => this.itemVariants.data = json);
+        .then(json => {
+          this.itemVariants.data = json;
+          this.itemData.variants.data = json;
+          this.itemData.variants.internal_names = json.reduce((arr, r) => {
+            if (!arr.includes(r.id)) arr.push(r.id);
+            return arr;
+          }, []);
+        });
+
+      fetch("./data/durability.json")
+        .then(response => response.json())
+        .then(json => this.itemData.durability = json);
+
+      fetch("./data/stack.json")
+        .then(response => response.json())
+        .then(json => this.itemData.stack = json);
+
+      fetch("./data/wrapping paper.json")
+        .then(response => response.json())
+        .then(json => this.itemData.wrappingPaper = json);
 
       window.onclick = event => {
         if (event.target == this.$refs['variant-selector']) {
@@ -159,27 +277,11 @@
       "pref.language.selected"(to) {
         fetch("./ACNH/item_ids/items_" + this.pref.language.selected + ".json")
           .then(response => response.json())
-          .then(json => this.itemData = json);
+          .then(json => this.itemData.translated = json);
         this.saveToLocalStorage();
       },
-      search(to) {
-        this.results = [];
-
-        if (to.length > 0) {
-          Object.entries(this.itemData).forEach(([key, val]) => {
-            this.results.push(...val.filter(item => item.name.toLowerCase().includes(to.toLowerCase())));
-          });
-          let internal_names = this.results.reduce((arr, r) => {
-            if (r.internal_name) arr.push(r.internal_name);
-            return arr;
-          }, []);
-
-          this.itemVariants.result = this.itemVariants.data.reduce((arr, v) => {
-            if (internal_names.includes(v.id)) arr.push(v.id);
-            return arr;
-          }, []);
-        }
-        this.saveToLocalStorage();
+      "search.text"(to) {
+        this.searchItems(to);
       },
       copyText(to) {
         if (to.length > 0) {
@@ -200,6 +302,7 @@
               t.hide();
           });
         });
+        this.saveToLocalStorage();
       },
       "selected.items.length"(to) {
         this.showAllToast();
